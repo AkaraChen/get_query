477f2751f921dc16ce8f93e58e10c14f52c68d7e
AkaraChen <akarachen@outlook.com>
2023-09-25 20:22:08 +0800

    test: remove unused code

--- test/core/query_test.dart
+++ test/core/query_test.dart
@@ -1,15 +1,10 @@
 // ignore_for_file: empty_catches
 
-import 'package:get_query/get_query.dart';
 import 'package:test/test.dart';
 
 import '../get_query_test.dart';
 
 void main() {
-  tearDown(() {
-    queryClient.clear();
-  });
-
   test('should be able to fetch data', () async {
     final controller = SimpleController();
     await controller.fetch();
